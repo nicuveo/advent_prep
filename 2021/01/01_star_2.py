2857c7d22c81063bc29c89336c621c08d62e6722
Antoine Leblanc <crucuny@gmail.com>
2022-06-21 11:30:14 +0100

    Update 01_star_2.py

--- 2021/01/01_star_2.py
+++ 2021/01/01_star_2.py
@@ -20,11 +20,55 @@ def parse_input_and_solve(filename):
     return number_of_increases
 
 
+
+# (Same comments as for part 1 re. opening the file in main.)
+#
+# There's a trick you can use here:
+#
+# previous window: [a, b, c]
+# current window:     [b, c, d]
+#
+# current window > previous window <=> d > a
+#
+# So you don't need to keep the sliding window!
+depths = map(int, file.readlines())
+number_of_increases = 0
+for i in range(3, len(depths)):
+    if depths[i] > depths[i-3]:
+        number_of_increases += 1 
+        
+# if you want to keep it, then, a few suggestions:
+#   - you don't need to append in both branches of the if;
+#   - you don't need to re-test for length before popping
+sliding_window = []
+for line in file:
+    num = int(line)
+    old_sum = sum(sliding_window)
+    sliding_window.append(num)
+    if len(sliding_window) < 3:
+        continue
+    sliding_window.pop(0)
+    new_sum = sum(sliding_window)
+    if new_sum > old_sum:
+        number_of_increases += 1
+        
+# if you want to keep it but only perform the minimal test:
+sliding_window = []
+for line in file:
+    num = int(line)
+    sliding_window.append(num)
+    if len(sliding_window) < 3:
+        continue
+    old = sliding_window.pop(0)
+    if num > old:
+        number_of_increases += 1
+
+
+
 def main():
     input_file_name = "../input"
     print(parse_input_and_solve(input_file_name))
     return -1
-
 
 if __name__ == "__main__":
     main()
